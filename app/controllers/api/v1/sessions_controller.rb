228bd08f7277edd23f2aafd1251da6611b46ec52
Catheryn Small <catheryn.small@gmail.com>
2021-02-21 21:29:06 -0500

    current user can be tracked

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,22 @@
 class Api::V1::SessionsController < ApplicationController
     def create
-        byebug
+        user = User.find_by(username: params[:session][:username])
+
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: {error: "Invalid Credentials"}
+        end
+    end
+
+    def get_current_user
+        if logged_in?
+            render json: current_user
+        else
+            render json: {
+                notice: "Not logged in."
+            }
+        end
     end
 end
