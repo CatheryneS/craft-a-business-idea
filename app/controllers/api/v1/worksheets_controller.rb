06f8de96602c5589808a4dd74474c4976c1dc7d6
Catheryn Small <catheryn.small@gmail.com>
2021-03-01 23:08:12 -0500

    add create method for worksheet

--- app/controllers/api/v1/worksheets_controller.rb
+++ app/controllers/api/v1/worksheets_controller.rb
@@ -6,6 +6,19 @@ class Api::V1::WorksheetsController < ApplicationController
     end
 
     def create
+        worksheet = Worksheet.new(user_id: params[:userId])
 
+        if worksheet.save
+            params[:sections][:loves].each{| love | worksheet.love.create!(content: love)}
+            params[:sections][:goodAts].each{| g | worksheet.good_ats.create!(content: g)}
+            params[:sections][:worldNeeds].each{| wn | worksheet.world_needs.create!(content: wn)}
+        end
+
+        if !worksheet.love.empty? && !worksheet.good_ats.empty? && !worksheet.world_needs.empty?
+            render json: worksheet, status: :created
+        else
+            resp = { error: worksheet.errors.full_messages.to_sentence}
+            render json: resp, status: :unprocessable_entity
+        end
     end
 end
